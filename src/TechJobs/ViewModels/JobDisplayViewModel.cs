799dda1842a931ff8d3c6049f8c5bc559e356856
PaulC2017 <paulc2017@att.net>
2018-03-09 05:48:19 -0500

    Added function to view Job Details based on ID (Job?>id = x)

---
+++ src/TechJobs/ViewModels/JobDisplayViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+using TechJobs.Data;
+
+namespace TechJobs.ViewModels
+{
+    public class JobDisplayViewModel
+    {
+        public string Name { get; set; }
+        public Employer Employer { get; set; }
+        public Location Location { get; set; }
+        public CoreCompetency CoreCompetency { get; set; }
+        public PositionType PositionType { get; set; }
+
+
+    }
+}
